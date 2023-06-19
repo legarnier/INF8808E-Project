1cdfac19508986c22c881a64b0077e454b8dc92a
Rezghool <r.ghalandari93@gmail.com>
2023-06-19 15:24:33 -0400

    save

--- code/Reza_vis.py
+++ code/Reza_vis.py
@@ -1,1 +1,46 @@
-#Start
+import plotly.graph_objects as go
+
+# Define the main values, maximum values, and minimum values
+main_values = [1, 2, 3, 4, 5]
+max_values = [2, 3, 4, 5, 6]
+min_values = [0, 1, 2, 3, 4]
+
+# Create the line graph with maximum and minimum values highlighted
+fig = go.Figure()
+
+# Add the main line
+fig.add_trace(go.Scatter(
+    x=list(range(len(main_values))),
+    y=main_values,
+    mode='lines',
+    name='Main Values'
+))
+
+# Add the shaded region between the maximum and minimum values
+fig.add_trace(go.Scatter(
+    x=list(range(len(max_values))),
+    y=max_values,
+    fill=None,
+    mode='lines',
+    line=dict(color='rgba(0,0,0,0)')
+))
+
+fig.add_trace(go.Scatter(
+    x=list(range(len(min_values))),
+    y=min_values,
+    fill='tonexty',
+    mode='lines',
+    name='Range',
+    line=dict(color='rgba(0,0,0,0)')
+))
+
+# Update layout
+fig.update_layout(
+    title='Line Graph with Highlighted Range',
+    xaxis=dict(title='X'),
+    yaxis=dict(title='Y'),
+    showlegend=True
+)
+
+# Display the graph
+fig.show()
