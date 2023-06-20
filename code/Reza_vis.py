0844f5c7cf12aa2cacb42939e7d2afdb1456a1ce
Rezghool <r.ghalandari93@gmail.com>
2023-06-20 12:48:22 -0400

    new file

--- code/Reza_vis.py
+++ code/Reza_vis.py
@@ -1,11 +1,71 @@
 import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
 
+def filter_just_time_latency(df):
+    '''
+        Add forecasting value to data
+
+        Args:
+            dataframe: The dataframe to process
+        Returns:
+            only one total latency for each city per time
+    '''
+    # TODO : filter data by time and city and return filtered data
+    #new_datafrom = df.groupby(['Site', 'Time'])['Total Latency','Forecast max',].first().reset_index()
+    #new_datafram ={}
+    #new_datafram['Forecast max avg w'] = df.groupby(['Site', 'Time'])['Latency'].mean().reset_index().rename(columns={'Latency': 'Average latency'})
+    #new_datafram['Forecast max avg'] = df.groupby(['Site', 'Time'])['Forecast max'].mean().reset_index().rename(columns={'Latency': 'Average latency '})
+
+
+
+    # Group by 'city' and calculate average of 'age'
+    new_datafram = df.groupby(['Site', 'Time']).mean().reset_index()
+    
+
+    new_datafram = new_datafram[['Site','Time','Latency','Forecast max','Forecast min','Confidence Level','Volatility']]
+    
+    # Create three new columns based on grouped results
+    #new_datafram = new_datafram.rename(columns={'age': 'average_age', 'salary': 'average_salary', 'height': 'average_height'})
+
+    print(new_datafram)
+    
+    
+    return new_datafram
+
+
+
+def add_forecasting(df):
+    '''
+        Add forecasting value to data
+
+        Args:
+            dataframe: The dataframe to process
+        Returns:
+            adding new value to current dataset related to forecasting. max min pretectid value, confidence level and Volatility
+    '''
+    # TODO : add new values to each row based on random formula
+    
+    # Specify the range for random values
+  
+    return df
 
 
 dataframe = pd.read_csv('dataset.csv')
 
+#print(dataframe[1])
 #preprocessing
+dataframe = filter_just_time_latency(dataframe)
+
+#print(dataframe)
+
+dataframe = add_forecasting(dataframe)
+
+#print(dataframe)
+
+
+
+
 
 
 
@@ -53,4 +113,4 @@ fig.update_layout(
 )
 
 # Display the graph
-fig.show()
+#fig.show()
