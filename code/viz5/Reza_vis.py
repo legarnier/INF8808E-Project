13a4619063342ad82e439b4a2c5c7e0e383a168f
Rezghool <80580733+Rezix93@users.noreply.github.com>
2023-06-21 14:52:47 -0400

    Update Reza_vis.py
    Update y axis value, update color and line color

--- code/viz5/Reza_vis.py
+++ code/viz5/Reza_vis.py
@@ -2,7 +2,7 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
-def filter_just_time_latency(df):
+def filter_groupby_time_city(df):
     '''
         Add forecasting value to data
 
@@ -27,8 +27,6 @@ def filter_just_time_latency(df):
     
     # Create three new columns based on grouped results
     #new_datafram = new_datafram.rename(columns={'age': 'average_age', 'salary': 'average_salary', 'height': 'average_height'})
-
-    print(new_datafram)
     
     
     return new_datafram
@@ -50,67 +48,127 @@ def add_forecasting(df):
   
     return df
 
+def graphV1(df) : 
+    # Define the main values, maximum values, and minimum values
+    main_values = [1, 2, 3, 4, 5]
+    max_values = [2, 3, 4, 5, 6]
+    min_values = [0, 1, 2, 3, 4]
 
-dataframe = pd.read_csv('dataset.csv')
+    x = dataframe['Time']
+    main_values = dataframe['Latency']
+    max_values = dataframe['Forecast max']
+    max_values = dataframe['Forecast min']
+
+    # Create the line graph with maximum and minimum values highlighted
+    fig = go.Figure()
+
+    # Add the main line
+    fig.add_trace(go.Scatter(
+        x=list(range(len(main_values))),
+        y=main_values,
+        mode='lines',
+        name='Main Values'
+    ))
+
+    # Add the shaded region between the maximum and minimum values
+    fig.add_trace(go.Scatter(
+        x=list(range(len(max_values))),
+        y=max_values,
+        fill=None,
+        mode='lines',
+        line=dict(color='rgba(0,0,0,0)')
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=list(range(len(min_values))),
+        y=min_values,
+        fill='tonexty',
+        mode='lines',
+        name='Range',
+        line=dict(color='rgba(0,0,0,0)')
+    ))
+
+    # Update layout
+    fig.update_layout(
+        title='Line Graph with Highlighted Range',
+        xaxis=dict(title='X'),
+        yaxis=dict(title='Y'),
+        showlegend=True
+    )
+
+    # Display the graph
+    fig.show()    
+
+
+
+def graphV2(df):
+    
+    x = df['Time']
+    y2 = df['Latency']
+    y1 = df['Forecast max']
+    y3 = df['Forecast min']
+
+    # create variable
+    
+    y_range = [0.5*min(min(y1), min(y2), min(y3)), 1.5*max(max(y1), max(y2), max(y3))]
+    rang_y_linecolor=dict(color='rgba(0,0,0,0)')
+    main_y_linecolor = dict(color='blue')
+    fillcolor = 'lightblue'
+    
+    # Create traces for each line
+    trace1 = go.Scatter(x=x, y=y1, mode='lines', name='Forecasting Max Latency', marker=dict(symbol='circle', size=8), fill = None,line = rang_y_linecolor)
+    trace2 = go.Scatter(x=x, y=y2, mode='lines+markers', name='Total Current Latency',fill='tonexty', fillcolor = fillcolor , line = main_y_linecolor)
+    trace3 = go.Scatter(x=x, y=y3, mode='lines', name='Forecasting Min Latency',fill='tonexty', fillcolor= fillcolor , line = rang_y_linecolor)
+
+    # Create data list
+    data = [trace1, trace2, trace3]
+
+    
+    
+    # Create layout
+    #layout = go.Layout(title='Line Chart', xaxis=dict(title='X'), yaxis=dict(title='Y', range=[0.5*min(min(y1), min(y2), min(y3)), 1.5*max(max(y1), max(y2), max(y3))]))
+    # Create layout
+    layout = go.Layout(
+    title='Line Chart',
+    xaxis=dict(title='Time',tickangle=45, tickmode='array', tickvals=x, ticktext=[str(val) for val in x]),
+    yaxis=dict(title='Latency',range= y_range)
+    )
+
+    # Create figure
+    fig = go.Figure(data=data, layout=layout)
+
+    fig.update_layout(
+        # on the y-axis
+        yaxis_ticksuffix=" ms",
+        # on the colorbar
+        coloraxis_colorbar_ticksuffix="m",
+        # To specify which tick should have suffix
+        yaxis_showticksuffix="all"  # or "first" or "last"
+    )
+    # Display the chart
+    fig.show()
+    
+    
+    
+
+dataframe = pd.read_csv('../../data/dataset.csv')
 
 #print(dataframe[1])
 #preprocessing
-dataframe = filter_just_time_latency(dataframe)
-
-#print(dataframe)
-
-dataframe = add_forecasting(dataframe)
+dataframe = filter_groupby_time_city(dataframe)
 
 #print(dataframe)
 
 
+Quebec_dataframe = dataframe.loc[dataframe['Site'] == 'Quebec']
+
+#print(Quebec_dataframe)
+
+graphV2(Quebec_dataframe)
 
 
 
 
 
 
-# Define the main values, maximum values, and minimum values
-main_values = [1, 2, 3, 4, 5]
-max_values = [2, 3, 4, 5, 6]
-min_values = [0, 1, 2, 3, 4]
 
-# Create the line graph with maximum and minimum values highlighted
-fig = go.Figure()
-
-# Add the main line
-fig.add_trace(go.Scatter(
-    x=list(range(len(main_values))),
-    y=main_values,
-    mode='lines',
-    name='Main Values'
-))
-
-# Add the shaded region between the maximum and minimum values
-fig.add_trace(go.Scatter(
-    x=list(range(len(max_values))),
-    y=max_values,
-    fill=None,
-    mode='lines',
-    line=dict(color='rgba(0,0,0,0)')
-))
-
-fig.add_trace(go.Scatter(
-    x=list(range(len(min_values))),
-    y=min_values,
-    fill='tonexty',
-    mode='lines',
-    name='Range',
-    line=dict(color='rgba(0,0,0,0)')
-))
-
-# Update layout
-fig.update_layout(
-    title='Line Graph with Highlighted Range',
-    xaxis=dict(title='X'),
-    yaxis=dict(title='Y'),
-    showlegend=True
-)
-
-# Display the graph
-#fig.show()
