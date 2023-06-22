0312fbc0f4669b24af4e1cd518e527d27d7122f9
Rezghool <r.ghalandari93@gmail.com>
2023-06-22 03:18:32 -0400

    add neew files. add my vis to main code

--- code/app.py
+++ code/app.py
@@ -1,12 +1,56 @@
-import dash
+import pathlib, dash
+from dash import Dash, dcc, html, Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import preprocess,vis5
+import pandas as pd
+import dash_bootstrap_components as dbc
+
+
 
 app = dash.Dash(__name__)
 
+
+
+# Get the data
+dataframe = pd.read_csv('../data/dataset.csv')
+
+
+
+#Get the vis5 
+vis5_df = preprocess.filter_groupby_time_city(dataframe)
+fig5 = vis5.initial(vis5_df)
+fig5.update_layout(height = 800, width = 1500)
+fig5.update_layout(dragmode = False)
+
+
+# Styling the sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# Padding for the page content
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+
+
+
+
+
 app.layout = html.Div(
     children=[
+
         html.H1("Latency Visualization", className="title"),
         html.Div(
             children=[
@@ -43,6 +87,13 @@ app.layout = html.Div(
                             n_clicks=0,
                             className="button-style",
                         ),
+                        
+                        dcc.Graph(id = 'fig5',figure = fig5),
+
+                        #html.Label('You can find forecasting visualization here:'),
+                        
+                    
+
                         html.Button(
                             "Visualization 6",
                             id="button-6",
@@ -62,6 +113,16 @@ app.layout = html.Div(
 
 
 @app.callback(
+    Output('fig5', 'style'),
+    [Input('button-5', 'n_clicks')]
+)
+def toggle_graph_visibility(n_clicks):
+    if n_clicks and n_clicks % 2 == 1:
+        return {'display': 'none'}  # Hide the graph
+    else:
+        return {'display': 'block'}  # Show the graph
+    
+@app.callback(
     Output("plot-text", "children"),
     Input("button-1", "n_clicks"),
     Input("button-2", "n_clicks"),
@@ -70,7 +131,7 @@ app.layout = html.Div(
     Input("button-5", "n_clicks"),
     Input("button-6", "n_clicks"),
 )
-def update_plot(*button_clicks):
+def update_plot(n_clicks):
     button_texts = [
         "Visualization 1",
         "Visualization 2",
@@ -79,9 +140,9 @@ def update_plot(*button_clicks):
         "Visualization 5",
         "Visualization 6",
     ]
-    triggered_button_id = dash.callback_context.triggered[0]["prop_id"].split(".")[
-        0]
+    triggered_button_id = dash.callback_context.triggered[0]["prop_id"].split(".")[0]
     if triggered_button_id:
-        button_index = int(triggered_button_id.split("-")[1]) - 1
-        return button_texts[button_index]
+       button_index = int(triggered_button_id.split("-")[1]) - 1
+       return button_texts[button_index]
+    
     return ""
