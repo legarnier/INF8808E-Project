fbc75630238100f42394ea3781e8219834542191
hastighaderazad98 <91502217+hastighaderazad98@users.noreply.github.com>
2023-06-24 13:52:05 -0400

    vis2.py completed

--- code/vis2.py
+++ code/vis2.py
@@ -24,5 +24,24 @@ def buble_chart(df):
     )
     return fig
 
-def line_chart(df):
-    pass
+def get_empty_figure():
+    fig = px.line(x=[0,0], y=[0,0])
+    fig.add_annotation(text='No data to display.<br> Select a bubble for more information.', xref='paper', yref='paper', showarrow=False) # src to center the text: https://codepen.io/etpinard/pen/WpmNEo
+    fig.update_layout(dragmode = False, xaxis = {'visible': False, 'showticklabels': False}, yaxis = {'visible': False, 'showticklabels': False}, plot_bgcolor='rgb(243, 243, 243)')
+                      
+    return fig
+
+def line_chart(df, application_type):
+    fig = px.line(df, x = "Time", y = "Latency")
+    fig.update_layout(
+        title=f'Latency for {application_type} over time',
+        xaxis=dict(
+            title='Time',
+        ),
+        yaxis=dict(
+            title='Latency',
+        ),
+        paper_bgcolor='rgb(243, 243, 243)',
+        plot_bgcolor='rgb(243, 243, 243)',
+    )
+    return fig
