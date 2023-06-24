00171c8e38075d8d132e9a7edda0e54311fe509f
Rezghool <80580733+Rezix93@users.noreply.github.com>
2023-06-23 20:38:37 -0400

    Update app.py
    update 1pp.py, new style

--- code/app.py
+++ code/app.py
@@ -1,189 +1,296 @@
-import pathlib, dash
-from dash import Dash, dcc, html, Input, Output
+import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import preprocess,vis5,viz4
+from dash import dcc, html
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
 import dash_bootstrap_components as dbc
+import preprocess,vis5,viz4
+from dash.dependencies import Input, Output
 
 
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
 # Get the data
 dataframe = pd.read_csv('../data/dataset.csv')
 
-
-
 #Get the vis5 
 vis5_df = preprocess.filter_groupby_time_city(dataframe)
 fig5 = vis5.initial(vis5_df)
 
-fig5.update_layout(height = 700, width = 1800)
+fig5.update_layout(height = 700, width = 1300)
 fig5.update_layout(autosize=True)
 
 
-fig4 = vis5.initial(dataframe)
-fig4.update_layout(height = 700, width = 1800)
-fig4.update_layout(autosize=True)
-
-
-
-<<<<<<< HEAD
-=======
-##
-
-
-# Styling the sidebar
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
->>>>>>> 01f5926 (update app.py)
-
-
-# Sidebar content
-sidebar = html.Div(
-    [
-        html.H2("INF8808", className="display-4"),
-        html.Hr(),
-        dbc.Nav(
-            [
-                dbc.NavLink("Dashboard", href="/", active="exact"),
-                dbc.NavLink("Transformer Performance", href="/page-1", active="exact"),
-                dbc.NavLink("Tap changer Performance", href="/page-2", active="exact"),
-                dbc.Button(
-                            "Visualization 5",
-                            id="button-5",
-                            n_clicks=0,
-                            className="button-style",
-                        )
-                        
-            ],
-            vertical=True,
-            pills=True,
-        ),
-        dcc.Graph(
-                                id = 'fig5',
-                                figure = fig5,
-                                style={'width': '%100', 'display': 'inline-block'}
-                        ),
-        dcc.Graph(
-                                id = 'fig5',
-                                figure = fig5,
-                                style={'width': '%100', 'display': 'inline-block'}
-                        ),
-      
-
-    ],
-    style=SIDEBAR_STYLE,
-)
-
-
-
-content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
-
-app.layout = html.Div([
-    dcc.Location(id="url"),
-    sidebar,
-    content
-    
-])
-
-
-
-
-
-
-"""
 app.layout = html.Div(
-    children=[
+    [
+        dbc.Row(
+            [
+                # Sidebar
+                dbc.Col(
+                    [
+                        html.H4("Graphs", className="sidebar-title",style={'position': 'fixed', 'width': '20%', 'height': '100vh'}),
+                        dbc.ListGroup(
+                            [
+                                dbc.ListGroupItem(
+                                    [
+                                        dbc.Button(
+                                            f"Graph {graph_id}",
+                                            id='button-' + str(graph_id),
+                                            color="primary",
+                                            outline=True,
+                                            style={"text-align": "left"},
+                                        ),
+                                        dbc.Collapse(
+                                            dcc.Graph(id={"type": "graph", "index": graph_id}),
+                                            id={"type": "collapse", "index": graph_id},
+                                            is_open=False,
+                                        ),
+                                    ]
+                                )
+                                for graph_id in range(1, 7)  # Update the range based on the number of graphs
+                            ],
+                            className="sidebar-content",
 
-        html.H1("Latency Visualization", className="title"),
-        html.Div(
-            children=[
-                html.Div(
-                    className="buttons",
-                    children=[
-                        html.Button(
-                            "Visualization 1",
-                            id="button-1",
-                            n_clicks=0,
-                            className="button-style",
                         ),
-                        html.Button(
-                            "Visualization 2",
-                            id="button-2",
-                            n_clicks=0,
-                            className="button-style",
-                        ),
-                        html.Button(
-                            "Visualization 3",
-                            id="button-3",
-                            n_clicks=0,
-                            className="button-style",
-                        ),
-                        html.Button(
-                            "Visualization 4",
-                            id="button-4",
-                            n_clicks=0,
-                            className="button-style",
-                        ),
-                        dcc.Graph(
-                                id = 'fig4',
-                                figure = fig4,
-                                style={'width': '%100', 'display': 'inline-block'}
-                        ),
-                        html.Button(
-                            "Visualization 5",
-                            id="button-5",
-                            n_clicks=0,
-                            className="button-style",
-                        ),
-                        
-                        dcc.Graph(
-                                id = 'fig5',
-                                figure = fig5,
-                                style={'width': '%100', 'display': 'inline-block'}
-                        ),
-
-                        #html.Label('You can find forecasting visualization here:'),
-                        
+                    ],
+                    width=3,
+                    style={
+                            'position': 'fixed',
+                            'top': 0,
+                            'height': '100vh',
+                            'width': '20%'
+                        },
+                    className="sidebar",
                     
+                ),
+                # Graphs
+                dbc.Col(
+                    [
+                        html.Div(
+                            [
+                                html.H1("INF8808 Final project", className="project-title"),
+                                html.H3("Team Number: 7", className="team-number"),
+                                html.H3("Summer 2023", className="date"),
+                            ],
+                            className="header-container",
+                            style={
+                                'position': 'relative',
+                                'top': 0,
+                                'left': '30%'
+                              },
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        dbc.Alert(
+                                            [
+                                                html.H4("Graph 1", className="graph-title"),
+                                                dcc.Graph(
+                                                    figure={"data": [{"y": [4, 2, 3, 1]}]},
+                                                    id='fig1'
+                                                ),
+                                            ],
+                                            id="graph-alert-1",
+                                            color="info",
+                                            dismissable=True,
+                                        ),
+                                    ],
+                                    width=12,
+                                   style={
+                                        'position': 'relative',
+                                        'top': 0,
+                                        'left': '30%'
+                                    },
+                                ),
+                            ]
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        dbc.Alert(
+                                            [
+                                                html.H4("Graph 2", className="graph-title"),
+                                                dcc.Graph(
+                                                    figure={"data": [{"y": [1, 3, 2, 4]}]},
+                                                    id='fig2'
+                                                ),
+                                            ],
+                                            id="graph-alert-2",
+                                            color="info",
+                                            dismissable=True,
+                                        ),
+                                    ],
+                                    width=12,
+                                    style={
+                                        'position': 'relative',
+                                        'top': 0,
+                                        'left': '30%'
+                                    },
+                                ),
+                            ]
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        dbc.Alert(
+                                            [
+                                                html.H4("Graph 3", className="graph-title"),
+                                                dcc.Graph(
+                                                    figure={"data": [{"y": [3, 1, 4, 2]}]},
+                                                    id='fig3'
+                                                ),
+                                            ],
+                                            id="graph-alert-3",
+                                            color="info",
+                                            dismissable=True,
+                                        ),
+                                    ],
+                                    width=12,
+                                    style={
+                                        'position': 'relative',
+                                        'top': 0,
+                                        'left': '30%'
+                                    },
+                                ),
+                            ]
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        dbc.Alert(
+                                            [
+                                                html.H4("Graph 4", className="graph-title"),
+                                                dcc.Graph(
+                                                    figure={"data": [{"y": [2, 4, 1, 3]}]},
+                                                    id='fig4'
+                                                ),
+                                            ],
+                                            id="graph-alert-4",
+                                            color="info",
+                                            dismissable=True,
+                                        ),
+                                    ],
+                                    width=12,
+                                    style={
+                                        'position': 'relative',
+                                        'top': 0,
+                                        'left': '30%'
+                                    },
+                                ),
+                            ]
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        dbc.Alert(
+                                            [
+                                                html.H4("Graph 5", className="graph-title"),
+                                                 dcc.Graph(
+                                                        id = 'fig5',
+                                                        figure = fig5,
+                                                        style={'width': '%100', 'display': 'inline-block'}
+                                                )
+                                                
+                                            ],
+                                            id="graph-alert-5",
+                                            color="info",
+                                            dismissable=True,
+                                        ),
+                                    ],
+                                    width=12,
+                                    style={
+                                        'position': 'relative',
+                                        'top': 0,
+                                        'left': '30%'
+                                    },
+                                ),
+                            ]
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        dbc.Alert(
+                                            [
+                                                html.H4("Graph 6", className="graph-title"),
+                                                dcc.Graph(
+                                                    figure={"data": [{"y": [1, 2, 4, 3]}]},
+                                                    id = 'fig6',
 
-                        html.Button(
-                            "Visualization 6",
-                            id="button-6",
-                            n_clicks=0,
-                            className="button-style",
+                                                ),
+                                            ],
+                                            id="graph-alert-6",
+                                            color="info",
+                                            dismissable=True,
+                                        ),
+                                    ],
+                                    width=12,
+                                    style={
+                                        'position': 'relative',
+                                        'top': 0,
+                                        'left': '30%'
+                                    },
+                                ),
+                            ]
                         ),
                     ],
+                    width=9,
+                    className="graph-area",
                 ),
-                html.Div(
-                    className="plot",
-                    children=[
-                        html.H1(id="plot-text", children=""),
-                    ],
-                )], style={"display": "flex",'width': '100%'}),
-    ],
+            ],
+            className="main-row",
+        ),
+    ]
 )
-"""
 
 
+@app.callback(
+    Output('fig1', 'style'),
+    [Input('button-1', 'n_clicks')]
+)
+def toggle_graph_visibility(n_clicks):
+    if n_clicks and n_clicks % 2 == 1:
+        return {'display': 'none'}  # Hide the graph
+    else:
+        return {'display': 'block'}  # Show the graph
 
-<<<<<<< HEAD
+@app.callback(
+    Output('fig2', 'style'),
+    [Input('button-2', 'n_clicks')]
+)
+def toggle_graph_visibility(n_clicks):
+    if n_clicks and n_clicks % 2 == 1:
+        return {'display': 'none'}  # Hide the graph
+    else:
+        return {'display': 'block'}  # Show the graph
+@app.callback(
+    Output('fig3', 'style'),
+    [Input('button-3', 'n_clicks')]
+)
+def toggle_graph_visibility(n_clicks):
+    if n_clicks and n_clicks % 2 == 1:
+        return {'display': 'none'}  # Hide the graph
+    else:
+        return {'display': 'block'}  # Show the grap
+@app.callback(
+    Output('fig4', 'style'),
+    [Input('button-4', 'n_clicks')]
+)
+def toggle_graph_visibility(n_clicks):
+    if n_clicks and n_clicks % 2 == 1:
+        return {'display': 'none'}  # Hide the graph
+    else:
+        return {'display': 'block'}  # Show the graph
 
-
-=======
->>>>>>> 01f5926 (update app.py)
 @app.callback(
     Output('fig5', 'style'),
     [Input('button-5', 'n_clicks')]
@@ -193,38 +300,17 @@ def toggle_graph_visibility(n_clicks):
         return {'display': 'none'}  # Hide the graph
     else:
         return {'display': 'block'}  # Show the graph
-
 @app.callback(
-    Output('fig4', 'style'),
-    [Input('button-4', 'n_clicks')]
-)   
+    Output('fig6', 'style'),
+    [Input('button-6', 'n_clicks')]
+)
 def toggle_graph_visibility(n_clicks):
     if n_clicks and n_clicks % 2 == 1:
         return {'display': 'none'}  # Hide the graph
     else:
-        return {'display': 'block'}  # Show the graph  
+        return {'display': 'block'}  # Show the graph
 
-@app.callback(
-    Output("plot-text", "children"),
-    Input("button-1", "n_clicks"),
-    Input("button-2", "n_clicks"),
-    Input("button-3", "n_clicks"),
-    Input("button-4", "n_clicks"),
-    Input("button-5", "n_clicks"),
-    Input("button-6", "n_clicks"),
-)
-def update_plot(n_clicks):
-    button_texts = [
-        "Visualization 1",
-        "Visualization 2",
-        "Visualization 3",
-        "Visualization 4",
-        "Visualization 5",
-        "Visualization 6",
-    ]
-    triggered_button_id = dash.callback_context.triggered[0]["prop_id"].split(".")[0]
-    if triggered_button_id:
-       button_index = int(triggered_button_id.split("-")[1]) - 1
-       return button_texts[button_index]
-    
-    return None
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
