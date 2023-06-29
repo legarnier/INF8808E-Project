26b6fc1af03bce8bfc41faf1789c4c89042b7d1a
Rezghool <r.ghalandari93@gmail.com>
2023-06-29 16:15:08 -0400

    add coment to viz5

--- code/vis5.py
+++ code/vis5.py
@@ -9,6 +9,9 @@ from IPython.display import display
 import ipywidgets as widgets
 import hover_template
 
+
+#In this function I add two box at the top of my graph to show two value: [last_confidence_level,last_volatility_level ]
+#I create a simaple shape by go.layout.shape and then put value there
 def addBoxes(fig,last_confidence_level,last_volatility_level,main_color,color_light) : 
         fig.update_layout(
                 shapes=[
@@ -63,6 +66,10 @@ def addBoxes(fig,last_confidence_level,last_volatility_level,main_color,color_li
         
 def update_vis5(df,city):
     
+    
+    #I have three diffrent city to show their latency forecast during time. for better visualization
+    #I create three difrent color for ecah city
+    # First I created the color then an array to have all colors
     light_red = 'lightpink'
     light_green =  'lightgreen'
     light_blue = 'lightblue'
@@ -73,26 +80,24 @@ def update_vis5(df,city):
     {"name": "Manitoba", "color": 'green', "color_light": light_green}
     ]
 
+    #Create a simple dataframe because it is easier to find value there by panda data structure:D
     colors_df = pd.DataFrame(colors)
     
+    
+    #I choose the color here based on city value. As I told before I selected a speceif color or ecah city.
     main_color = colors_df.query(f"name == '{city}'")['color'].values[0]
     color_light = colors_df.query(f"name == '{city}'")['color_light'].values[0]
 
     
+    # For my graph I need to show the main current latency, then highlight between max an min forecast value to show the range.
+    # based on my dataset, 
     x = df['Time']
     y_main = df['Latency']
     y_max = df['Forecast max']
     y_min = df['Forecast min']
 
-
     x_len = len(x)
-    mid_index = len(x) // 2
 
-    #x_half = x[:mid_index]
-    #y_max_half = y_max[:mid_index]
-    #y_main_half = y_main[:mid_index]
-    #y_min_half = y_min[:mid_index]
-    
     
     confidence_level = df['Confidence Level']
     volatility_level = df['Volatility']
@@ -100,27 +105,29 @@ def update_vis5(df,city):
     last_confidence_level = round(confidence_level.iloc[x_len-1],2)
     last_volatility_level = round(volatility_level.iloc[x_len-1],2)
 
+    #For hover tempalte  we need some values, so I created a array to use it in then
     hover_text = hover_template.hover_vis5_forcasting_range(y_min, y_main, y_max, confidence_level, volatility_level)
 
-    # create variable
-    
+    # To have a better veiw of my graph I decieded to have adynamic range of bigest value and minest value then use a coeffiecnt of that value.
+    #I mean instead of draw graph from shortest to largest value show my favourite range
     y_range = [0.8*min(min(y_max), min(y_main), min(y_min)), 1.2*max(max(y_max), max(y_main), max(y_min))]
     x_range = [min(x), max(x)]
 
     rang_y_linecolor=dict(color='rgba(0,0,0,0)')
     
+    #Assign the color selected before. I have a set of colro for each city
     main_y_linecolor = dict(color= main_color)
     fillcolor = color_light
     
    
-    
+    #Here I create the layout.
     layout = go.Layout(
         height=700,
         title={
             'text':'',
             'xanchor': 'center',
             'yanchor': 'top',
-            'font': {'size': 30},  # Increase the font size to 24,
+            'font': {'size': 30},  # Increase the font size to 30,
             'y': 0.995 , # Adjust the y coordinate to position the title higher
             'x': 0.5,  # Adjust the vertical position of the title if needed
             },
@@ -135,7 +142,7 @@ def update_vis5(df,city):
         yaxis=dict(title='Latency',range= y_range),
         
             # on the y-axis
-            yaxis_ticksuffix=" ms",
+            yaxis_ticksuffix=" ms", #add suffix to each y value index
             # on the colorbar
             coloraxis_colorbar_ticksuffix="m",
             # To specify which tick should have suffix
@@ -157,7 +164,9 @@ def update_vis5(df,city):
     addBoxes(fig,last_confidence_level,last_volatility_level,main_color,color_light)
     
         
-    # Create traces for each line
+    # Create traces for each line 
+    # As i told before I have three difrent values. main latency, max and min values.
+    # I use fill properties to fullfil my goal. I put fill = none for max value . "tonexty" for middle value then "tonexty" for min value.
     trace1 = go.Scatter(x=x, y=y_max, mode='lines', marker=dict(symbol='circle', size=8), fill = None,line = rang_y_linecolor, hoverinfo="skip")
     trace2 = go.Scatter(x=x, y=y_main, mode='lines+markers', name='Total Current Latency',fill='tonexty', fillcolor = fillcolor , 
                         line = main_y_linecolor,
@@ -167,8 +176,8 @@ def update_vis5(df,city):
                         )
     trace3 = go.Scatter(x=x, y=y_min, mode='lines', name='Forecasting Min Latency',fill='tonexty', fillcolor= fillcolor , line = rang_y_linecolor,hoverinfo="skip")
 
+    # Here I try to show that last 10 percent of the graph are preticted so I draw  dash line graph for last 10 percent of the graph.
     x_index = int(x_len * 0.90)
-    
     trace2_1 = go.Scatter(x=x[x_index:],
                           y=y_main[x_index:],
                           hoverinfo="skip",
