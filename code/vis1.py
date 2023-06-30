b5df8e6a75c27040b66bcffe35ea35e9c3faaa1f
Mahsa <67243063+MahsaPaziraei@users.noreply.github.com>
2023-06-29 20:30:00 -0400

    Update vis1.py

--- code/vis1.py
+++ code/vis1.py
@@ -1,56 +1,97 @@
+import pandas as pd
+import preprocess
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+# Get the data
+dataframe = pd.read_csv('../data/dataset.csv')
+df_dense = pd.read_csv(('../data/dense_dataset.csv'))
+df_dense['Time'] = pd.to_datetime(df_dense['Time'])
+
+# Get the vis1
+dict_data_site = preprocess.filter_by_Site(dataframe)
+
+Qc_by_type = preprocess.filter_by_type(dict_data_site['Quebec'])
+On_by_type = preprocess.filter_by_type(dict_data_site['Ontario'])
+Man_by_type = preprocess.filter_by_type(dict_data_site['Manitoba'])
+
+QC_latency_network = Qc_by_type['network']
+QC_latency_app = Qc_by_type['app']
+
+ON_latency_network = On_by_type['network']
+ON_latency_app = On_by_type['app']
+
+MAN_latency_network = Man_by_type['network']
+MAN_latency_app = Man_by_type['app']
+
+QC_latency_network_df = preprocess.Protocol_to_df(QC_latency_network)
+ON_latency_network_df = preprocess.Protocol_to_df(ON_latency_network)
+MAN_latency_network_df = preprocess.Protocol_to_df(MAN_latency_network)
 
 
-def gauge_chart(variables,values):
- 
-    fig = go.Figure()
-    # need to define types of subplots...
-    fig = make_subplots(
-        rows=len(values),
-        cols=len(variables),
-        specs=[[{"type": "indicator"} for c in variables] for t in values],
-    )
-    for i in range(len(variables)):
-        domain_start = i * (1 / 6)
-        domain_end = (i + 1) * (1 / 6)
-        domain_center = (domain_start + domain_end) / 2
+QC_latency_app_df = preprocess.Protocol_to_df(QC_latency_app)
+ON_latency_app_df = preprocess.Protocol_to_df(ON_latency_app)
+MAN_latency_app_df = preprocess.Protocol_to_df(MAN_latency_app)
 
-        fig.add_trace(go.Indicator(
-            mode="gauge+number+delta",
-            value=values[i],
-            title={'text': variables[i]},
-            delta={'reference': 65},
-            domain={'row': 0, 'column': i},
-            gauge={'axis': {'range': [None, 100]},
-                'bar': {'color': 'rgb(0, 128, 255)'},
-                'bgcolor': 'white',
-                'borderwidth': 2,
-                'bordercolor': 'gray',
-                'steps': [
-                    {'range': [0, values[i]], 'color': 'rgb(0, 70, 140)'},  # Couleur rouge pour l'augmentation
-                    {'range': [values[i], 100], 'color': 'rgb(0, 128, 255)'}],  # Couleur bleue pour le reste
+Qc_by_type['network'] = QC_latency_network_df
+On_by_type['network'] = ON_latency_network_df
+Man_by_type['network'] = MAN_latency_network_df
 
-                }))
+Qc_by_type['app'] = QC_latency_app_df
+On_by_type['app'] = ON_latency_app_df
+Man_by_type['app'] = MAN_latency_app_df
 
-        fig.add_annotation(
-            x=domain_center,
-            y=1,
-            text='',
-            showarrow=False,
-            textangle=0,
-            xanchor='center',
-            yanchor='middle',
-            font=dict(size=12, color='black'),
-            opacity=0.8
+dict_data_site['Quebec'] = Qc_by_type
+dict_data_site['Ontario'] = On_by_type
+dict_data_site['Manitoba'] = Man_by_type
+
+data = dict_data_site['Quebec']['app']
+
+# Variables pour les jauges et les boutons radio
+variables = ['HTTP', 'HTTPS', 'TCP', 'ICMP', 'TWAMP', 'UDP']
+num_gauges = len(variables)
+default_row_index = 0
+
+layout =html.Div(
+    children=[
+        html.H5("Site "),
+        dbc.Button(
+            "Quebec",
+            id="button-site-qc",
+            color="primary",
+            className="mr-1"
+        ),
+        dbc.Button(
+            "Ontario",
+            id="button-site-on",
+            color="primary",
+            className="mr-1"
+        ),
+        dbc.Button(
+            "Manitoba",
+            id="button-site-man",
+            color="primary",
+            className="mr-1"
+        ),
+        html.H5("Protocol Type "),
+        dcc.RadioItems(
+            id="radio-type",
+            options=[
+                {"label": " Application ", "value": "app"},
+                {"label": " Network ", "value": "network"}
+            ],
+            value="app",
+            className="mb-2"
+        ),
+        html.Div(
+            id="gauges-container",
+            style={"display": "flex", "justify-content": "center"}
+        ),
+        dcc.Interval(
+            id="interval-component",
+            interval=3000,
+            n_intervals=0
         )
-
-    fig.update_layout(
-        grid={'rows': 1, 'columns': len(variables), 'pattern': "independent"},
-        height=230,
-        width=1200,  # Ajouter de la marge autour de la figure
-        template='plotly_white',  # Utiliser un thème de couleur clair
-    )
-
-    return(fig)
+    ]
+)
